52e389c7712ab29e6c2426fd7abbfb80ac217728
hojland <36692171+Hojland@users.noreply.github.com>
2022-04-27 13:59:50 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -33,7 +33,8 @@ if [ -f "${config}/config.yml" ] && [ -f "${config}/CHANGELOG.tpl.md" ]; then
   echo "::debug::git-chlog: -n '${next_tag}'"
   echo "::debug::git-chlog: -o '${output}'"
   echo "::debug::git-chlog: -t '${tag}'"
-  echo "::info ::git-chlog executing command: /usr/local/bin/git-chglog --config "${config}/config.yml" ${next_tag} ${tag}"
+  git fetch --all --tags
+  echo "::info::git-chlog executing command: /usr/local/bin/git-chglog --config "${config}/config.yml" ${next_tag} ${tag}"
 
   changelog=$(/usr/local/bin/git-chglog --config "${config}/config.yml" ${next_tag} ${tag})
 
@@ -41,7 +42,7 @@ if [ -f "${config}/config.yml" ] && [ -f "${config}/CHANGELOG.tpl.md" ]; then
   echo "${changelog}"
   echo "----------------------------------------------------------"
 
-  echo "::debug ::git-chlog: -o '${output}'"
+  echo "::debug::git-chlog: -o '${output}'"
   if [[ ! -z "$output" ]]; then
     echo "::debug ::git-chlog -o options is set. writing changelog to ${output}"
     echo "${changelog}" > ${output}
